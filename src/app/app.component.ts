801e0630af15d17a780976bb63bffaccfe0aff4f
muenchto <tobi.muench@gmail.com>
2025-03-23 23:01:34 +0100

    add files from official tutorial lint it change to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: ``,
-  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img
+            class="brand-logo"
+            src="/assets/logo.svg"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'homes';
+}
