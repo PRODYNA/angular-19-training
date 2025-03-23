c37ef8d5a90bcd68c2cd121325802546cca73d4f
muenchto <tobi.muench@gmail.com>
2025-03-23 22:12:19 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: ``,
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
