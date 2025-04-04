4a95eb6b815ac079c8ae2a1f3fa8b89dead0c8d7
muenchto <tobi.muench@gmail.com>
2025-04-04 16:00:16 +0200

    add unit tests simple and using housing service mock

--- src/app/components/listing-apply/listing-apply.component.spec.ts
+++ src/app/components/listing-apply/listing-apply.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListingApplyComponent } from './listing-apply.component';
+import { HousingService } from '../../services/housing.service';
 
 describe('ListingApplyComponent', () => {
   let component: ListingApplyComponent;
@@ -8,6 +9,7 @@ describe('ListingApplyComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ListingApplyComponent],
+      providers: [{ provide: HousingService, useClass: MockHousingService }],
     }).compileComponents();
   });
 
@@ -20,4 +22,53 @@ describe('ListingApplyComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create a form with three controls', () => {
+    expect(component.applyForm.contains('firstName')).toBeTruthy();
+    expect(component.applyForm.contains('lastName')).toBeTruthy();
+    expect(component.applyForm.contains('email')).toBeTruthy();
+  });
+
+  it('should call housing service to submit application', () => {
+    // Given
+    component.applyForm.setValue({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+    });
+
+    const housingService: MockHousingService = TestBed.inject(HousingService);
+    const submitSpy = spyOn(housingService, 'submitApplication');
+
+    // When
+    component.submitApplication();
+
+    // Then
+    expect(submitSpy).toHaveBeenCalled();
+    expect(submitSpy).toHaveBeenCalledWith(
+      'John',
+      'Doe',
+      'john.doe@example.com'
+    );
+  });
+
+  it('should call submit method on form submit', () => {
+    // Given
+    const submitSpy = spyOn(component, 'submitApplication');
+
+    // When
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('submit'));
+
+    // Then
+    expect(submitSpy).toHaveBeenCalled();
+  });
 });
+
+class MockHousingService {
+  public submitApplication(
+    firstName: string,
+    lastName: string,
+    email: string
+  ) {}
+}
