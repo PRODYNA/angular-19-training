809e18148871f36e4218af106a91a110730172e5
muenchto <tobi.muench@gmail.com>
2025-04-05 18:56:23 +0200

    implement tests for add-to-favourites component

--- src/app/components/add-to-favourites/add-to-favourites.component.spec.ts
+++ src/app/components/add-to-favourites/add-to-favourites.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddToFavouritesComponent } from './add-to-favourites.component';
+import { HousingService } from '../../services/housing.service';
+import { Observable, of } from 'rxjs';
+import { HousingLocation } from '../../types/housinglocation';
 
 describe('AddToFavouritesComponent', () => {
   let component: AddToFavouritesComponent;
@@ -8,6 +11,7 @@ describe('AddToFavouritesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AddToFavouritesComponent],
+      providers: [{ provide: HousingService, useClass: MockHousingService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddToFavouritesComponent);
@@ -20,10 +24,50 @@ describe('AddToFavouritesComponent', () => {
   });
 
   it('should call housing service to update fav status on button click', () => {
-    //TODO: Implement this test
+    // Given
+    const housingService: MockHousingService = TestBed.inject(HousingService);
+    const updateHousingLocationFavStatusSpy = spyOn(
+      housingService,
+      'updateHousingLocationFavStatus'
+    );
+    const housingId = 1;
+
+    component.housingId = housingId;
+    const button = fixture.nativeElement.querySelector('button');
+
+    // When
+    button.click();
+
+    // Then
+    expect(updateHousingLocationFavStatusSpy).toHaveBeenCalledWith(
+      housingId,
+      true
+    );
   });
 
   it('should update value returned by service when update called', () => {
-    // TODO: Implement this test
+    // Given
+    const housingService: MockHousingService = TestBed.inject(HousingService);
+    spyOn(housingService, 'updateHousingLocationFavStatus').and.returnValue(
+      of({ favourite: true } as HousingLocation)
+    );
+    const housingId = 1;
+
+    component.housingId = housingId;
+
+    // When
+    component.update();
+
+    // Then
+    expect(component.isFavouriteSignal()).toBe(true);
   });
 });
+
+class MockHousingService {
+  updateHousingLocationFavStatus(
+    id: number,
+    isFavourte: boolean
+  ): Observable<HousingLocation> {
+    return of();
+  }
+}
